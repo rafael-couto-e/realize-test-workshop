577324d8dd643a527d5a936c16f22a76b9ef18f7
Eder Martins <contato@eder.poa.br>
2021-05-14 15:11:15 -0300

    Adiciona teste de repository

--- app/src/test/java/br/eti/rafaelcouto/testworkshop/data/repository/GitHubRepositoryTest.kt
+++ app/src/test/java/br/eti/rafaelcouto/testworkshop/data/repository/GitHubRepositoryTest.kt
@@ -1,4 +1,44 @@
 package br.eti.rafaelcouto.testworkshop.data.repository
 
+import br.eti.rafaelcouto.testworkshop.data.api.GitHubApi
+import br.eti.rafaelcouto.testworkshop.data.model.Profile
+import br.eti.rafaelcouto.testworkshop.data.repository.abs.GitHubRepositoryAbs
+import com.google.common.truth.Truth
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockkClass
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
 class GitHubRepositoryTest {
+    private lateinit var mockApi: GitHubApi
+    private lateinit var sut: GitHubRepositoryAbs
+
+    @Before
+    fun start() {
+        mockApi = mockkClass(GitHubApi::class)
+        sut = GitHubRepository(mockApi)
+    }
+
+
+    @Test
+    fun getProfile() = runBlocking {
+        val userName = "userName"
+        val expected = Profile().apply {
+            name = "Test"
+            company = ""
+            blog = ""
+            location = ""
+            followers = 5
+            following = 5
+        }
+
+        coEvery { mockApi.getProfile(any()) }.returns(expected)
+        val result = sut.getProfile(userName)
+        coVerify { mockApi.getProfile(userName) }
+
+        Truth.assertThat(result.name).isEqualTo(expected.name)
+    }
 }
