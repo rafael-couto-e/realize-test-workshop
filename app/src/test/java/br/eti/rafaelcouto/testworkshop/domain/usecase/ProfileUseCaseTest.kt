22f313bbc78eb6c5d0640b51f65e2dce19068772
Hédie <hedie.strokes@gmail.com>
2021-05-21 15:13:52 -0300

    Implemeta sucesso e erro ProfileUseCaseTest.kt

--- app/src/test/java/br/eti/rafaelcouto/testworkshop/domain/usecase/ProfileUseCaseTest.kt
+++ app/src/test/java/br/eti/rafaelcouto/testworkshop/domain/usecase/ProfileUseCaseTest.kt
@@ -1,23 +1,69 @@
 package br.eti.rafaelcouto.testworkshop.domain.usecase
 
+import br.eti.rafaelcouto.testworkshop.data.model.Profile
+import br.eti.rafaelcouto.testworkshop.data.repository.abs.GitHubRepositoryAbs
+import br.eti.rafaelcouto.testworkshop.domain.mapper.abs.ProfileMapperAbs
+import br.eti.rafaelcouto.testworkshop.domain.uimodel.ProfileUI
+import com.google.common.truth.Truth
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockkClass
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import retrofit2.HttpException
 
 class ProfileUseCaseTest {
+    private lateinit var sut: ProfileUseCase
+    private lateinit var repository: GitHubRepositoryAbs
+    private lateinit var mapper: ProfileMapperAbs
 
     @Before
     fun setUp() {
-
+        repository = mockkClass(GitHubRepositoryAbs::class)
+        mapper = mockkClass(ProfileMapperAbs::class)
+        sut = ProfileUseCase(repository, mapper)
     }
 
     @Test
-    fun getProfileSuccessTest() {
+    fun getProfileSuccessTest() = runBlocking {
+        val userName = "userName"
+        val input = Profile().apply {
+            name = "Test"
+            company = ""
+            blog = ""
+            location = ""
+            followers = 5
+            following = 5
+        }
 
+        val expected = ProfileUI(
+                "Test",
+                "",
+                "",
+                "",
+                "5",
+                "5",
+                "5",
+                "2021-05-05"
+        )
+
+        coEvery { repository.getProfile(any()) }.returns(input)
+        every { mapper.map(any()) }.returns(expected)
+
+        val result = sut.getUserProfile(userName)
+        coVerify { repository.getProfile(userName) }
+
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
-    @Test(expected = HttpException::class)
+    @Test(expected = Exception::class)
     fun getProfileErrorTest() {
-
+        val userName = "userName"
+        coEvery { repository.getProfile(any()) }.throws(Exception())
+        runBlocking {
+            sut.getUserProfile(userName)
+        }
     }
 }
